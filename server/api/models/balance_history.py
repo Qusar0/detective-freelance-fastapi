ee480cd973c04a0c25e399ca542d185556d7aa06
AntonSeredenkov <seredenkov.anton2003@gmail.com>
2025-06-16 13:24:55 +0300

    BalanceHistory вынесен в отдельный файл

---
+++ server/api/models/balance_history.py
@@ -0,0 +1,38 @@
+import datetime
+from typing import Optional
+
+from sqlalchemy import (
+    DateTime,
+    ForeignKey,
+    Double,
+    Integer,
+    String,
+)
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+
+from server.api.models.models import Base
+
+
+class BalanceHistory(Base):
+    __tablename__ = 'balance_history'
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+    transaction_type: Mapped[Optional[str]] = mapped_column(String(30))
+    amount: Mapped[Optional[float]] = mapped_column(Double(53))
+    timestamp: Mapped[Optional[datetime.datetime]] = mapped_column(DateTime)
+    query_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey('user_queries.query_id', ondelete='CASCADE'),
+        nullable=False,
+    )
+
+    query: Mapped['UserQueries'] = relationship(
+        'UserQueries',
+        back_populates='balance_histories',
+    )
+
+    def __str__(self):
+        return f"История баланса ({self.id})"
