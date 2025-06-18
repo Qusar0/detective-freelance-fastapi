2dca820a083370470eb4e2c91db2ef192ce99a59
Saenwint <burntoutblunt@mail.ru>
2025-06-18 18:23:28 +0300

    flake8 file_storage

--- server/api/models/models.py
+++ server/api/models/models.py
@@ -404,11 +404,11 @@ class Countries(Base):
 
 class CountryLanguage(Base):
     __tablename__ = 'country_languages'
-    
+
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     country_id: Mapped[int] = mapped_column(ForeignKey('countries.id'))
     language_id: Mapped[int] = mapped_column(ForeignKey('languages.id'))
-    
+
     country: Mapped['Countries'] = relationship(back_populates='language_links')
     language: Mapped['Language'] = relationship(back_populates='country_links')
 
