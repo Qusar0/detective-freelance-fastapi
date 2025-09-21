1f637fb12e039925faff468c7d9326d3cc25d48c
Ваше Имя <you@example.com>
2025-09-21 21:01:49 +0300

    exception fix

--- server/api/dao/user_balances.py
+++ server/api/dao/user_balances.py
@@ -49,6 +49,11 @@ class UserBalancesDAO(BaseDAO):
             }
 
             await publish_event(channel, event_data)
+        except InsufficientFundsError:
+            raise InsufficientFundsError(
+                    current_balance=user_balance.balance,
+                    required_amount=amount
+                )
         except (SQLAlchemyError, Exception) as e:
             logger.error(f"Ошибка при списании с баланса: {e}")
             await db.rollback()
