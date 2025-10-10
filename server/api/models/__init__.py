fb3e5a717983aad796ba63fa335df4029c8d2534
Sergey-pixel-cell <kurin_0@mail.ru>
2025-10-10 18:27:14 +0300

    flake

--- server/api/models/__init__.py
+++ server/api/models/__init__.py
@@ -1,2 +1,2 @@
-from server.api.models.models import UserQueries
-from server.api.models.irbis_models import IrbisPerson
+from server.api.models.models import UserQueries  # noqa: F401
+from server.api.models.irbis_models import IrbisPerson  # noqa: F401
