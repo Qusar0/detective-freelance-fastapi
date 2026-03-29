f628d934f94636610d9cbf249dc7309837cb1d0f
askovalev <askovalev@satel.org>
2026-03-29 13:55:18 +0300

    Добавлена история баланса

--- server/api/models/__init__.py
+++ server/api/models/__init__.py
@@ -1,2 +1,10 @@
-from server.api.models.models import UserQueries  # noqa: F401
-from server.api.models.irbis_models import IrbisPerson  # noqa: F401
+import sys
+
+try:
+    from server.api.models.models import UserQueries  # noqa: F401
+    from server.api.models.irbis_models import IrbisPerson  # noqa: F401
+
+    sys.modules.setdefault('api.models.models', sys.modules.get('server.api.models.models'))
+    sys.modules.setdefault('api.models.irbis_models', sys.modules.get('server.api.models.irbis_models'))
+except ImportError:
+    pass
