f84ce0d05fc97b15d8525f5ae583fbd48056f1e4
askovalev <askovalev@satel.org>
2026-05-12 23:07:54 +0300

    Правка статики

--- server/__main__.py
+++ server/__main__.py
@@ -44,4 +44,4 @@ async def startup_event():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_config=None)
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_config=None, proxy_headers=True, forwarded_allow_ips="*")
